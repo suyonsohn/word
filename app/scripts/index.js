bc752ff16c688b3a012ae4208ea2b01990f0ad1a
sys <suyonsohn@gmail.com>
2018-10-06 08:22:56 -0700

    Interact with web3.js and display campaign details from smart contract to frontend

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -6,111 +6,76 @@ import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metaCoinArtifact from '../../build/contracts/MetaCoin.json'
+const campaignArtifacts = require('../../build/contracts/Campaign.json')
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-const MetaCoin = contract(metaCoinArtifact)
-
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-let accounts
-let account
+// Campaign is usable abstraction.
+const Campaign = contract(campaignArtifacts)
 
 const App = {
-  start: function () {
-    const self = this
+    start: function () {
+        const self = this
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider)
+        // Bootstrap the Campaign abstraction for Use.
+        Campaign.setProvider(web3.currentProvider)
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
-      }
+        console.log('working')
 
-      if (accs.length === 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
-        return
-      }
+        renderCampaign()
+    }
+}
 
-      accounts = accs
-      account = accounts[0]
+const renderCampaign = () => {
+    let instance
+    let totalCount
 
-      self.refreshBalance()
+    return Campaign.deployed().then((i) => {
+        instance = i
+        return instance.promiseIndex.call().then((total) => {
+            totalCount = total.toNumber()
+            console.log(`TOTAL PROMISE COUNT: ${totalCount}`)
+            for (let c = 1; c <= totalCount; c++) {
+                renderPromise(instance, c)
+            }
+        })
     })
-  },
+}
 
-  setStatus: function (message) {
-    const status = document.getElementById('status')
-    status.innerHTML = message
-  },
-
-  refreshBalance: function () {
-    const self = this
-
-    let meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.getBalance.call(account, { from: account })
-    }).then(function (value) {
-      const balanceElement = document.getElementById('balance')
-      balanceElement.innerHTML = value.valueOf()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error getting balance; see log.')
+const renderPromise = (instance, index) => {
+    instance.getPromise(index).then((p) => {
+        console.log(p)
+        $('#candidate-first-name').append(p[1])
+        $('#candidate-last-name').append(p[2])
+        $('#party-name').append(p[3])
+        $('#election-type').append(p[4])
+        $('#promise').append(p[5])
     })
-  },
-
-  sendCoin: function () {
-    const self = this
-
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-
-    this.setStatus('Initiating transaction... (please wait)')
-
-    let meta
-    MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.sendCoin(receiver, amount, { from: account })
-    }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
-    }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
-    })
-  }
 }
 
 window.App = App
 
 window.addEventListener('load', function () {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn(
-      'Using web3 detected from external source.' +
-      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
-      ' ensure you\'ve configured that source properly.' +
-      ' If using MetaMask, see the following link.' +
-      ' Feel free to delete this warning. :)' +
-      ' http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider)
-  } else {
-    console.warn(
-      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
-      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
-      ' Consider switching to Metamask for development.' +
-      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
-  }
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        console.warn(
+            'Using web3 detected from external source.' +
+            ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
+            ' ensure you\'ve configured that source properly.' +
+            ' If using MetaMask, see the following link.' +
+            ' Feel free to delete this warning. :)' +
+            ' http://truffleframework.com/tutorials/truffle-and-metamask'
+        )
+        // Use Mist/MetaMask's provider
+        window.web3 = new Web3(web3.currentProvider)
+    } else {
+        console.warn(
+            'No web3 detected. Falling back to http://127.0.0.1:8545.' +
+            ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
+            ' Consider switching to Metamask for development.' +
+            ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+        )
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
+    }
 
-  App.start()
+    App.start()
 })
